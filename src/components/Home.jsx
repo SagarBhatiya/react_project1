b783818c3beb632c8601132f2b19514f895e214a
Sagar Bhatiya <sagarbhatiya12211@gmail.com>
2025-02-03 18:19:01 +0530

    second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,40 @@
 import { Link } from "react-router-dom";
+import React, { useContext } from 'react';
+
 import Nav from "./Nav";
 
+import{ProductContext} from '../utils/Context'
+import Loading from "./loading";
 function Home() {
-  return (
+const [products] = useContext(ProductContext)
+  return ( 
+    products?
     <>
       <Nav />
       <div className=" w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
-        <Link
+        
+        {products.map((p,i)=>
+        (
+          <Link
           to="/details/1"
           className="mr-3 mb-3 card p-3 bg-center border shadow rounded w-[18%] h-[30vh] flex justify-center items-center flex-col"
         >
           <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat"
+            className="hover:scale-110  mb-3 w-full h-full bg-contain bg-no-repeat"
             style={{
               backgroundImage:
-                "URL(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
+                `url(${p.image})`,
             }}
           ></div>
-          <h6 className="text-black hover:text-blue-300">
-            Lorem ipsum dolor sit amet.
+          <h6 className="text-sm text-black hover:text-blue-300">
+            {p.title}
           </h6>
         </Link>
+        )
+        )}
+    
       </div>
-    </>
+    </> :<Loading/>
   );
 }
 
