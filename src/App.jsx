c92741b61dd47046511c275b960b04f44974e746
Sagar Bhatiya <sagarbhatiya12211@gmail.com>
2025-02-06 16:41:21 +0530

    06 feb 2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.css";
 import Home from "./components/Home";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 import Details from "./Details";
 
 function App() {
+  const { search, pathname } = useLocation();
   return (
     <>
       <div className="h-screen w-screen bg-white flex">
+        {(pathname != "/" || search.length > 0) && (
+          <Link to="/" className="text-red-100 absolute left-[20%] top-[3%]">
+            Home
+          </Link>
+        )}
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/details/:id" element={<Details />} />
